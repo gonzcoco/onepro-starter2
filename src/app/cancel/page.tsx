1ef3cafa657add3ede707836a518fab439098b1e
onepro-starter <goncalves.cyrille13@yahoo.fr>
2025-09-12 22:42:33 +0200

    Create page.tsx

---
+++ src/app/cancel/page.tsx
@@ -0,0 +1,8 @@
+export default function CancelPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Paiement annulé ❌</h1>
+      <p>Vous avez annulé le paiement. Vous pouvez réessayer quand vous voulez.</p>
+    </main>
+  );
+}
